ff98a2e04aa2ffa337b9aa47a98dd40a75f2f21b
Perryyyyyyyy <qiu13328224715@outlook.com>
2025-10-24 16:21:25 -0400

    feat(parallelogram): implement area() via cross product; update vertex comments

--- src/lab5/Parallelogram.java
+++ src/lab5/Parallelogram.java
@@ -1,10 +1,10 @@
 package lab5;
 
 public class Parallelogram {
-    Point A; // bottom-left
-    Point B; // top-left
-    Point C; // top-right
-    Point D; // bottom-right
+   	Point A; //assume bottom left is A
+	Point B; //assume top left is B
+	Point C; //assume top right is C
+	Point D; //assume bottom right is D
 	
 	public Parallelogram(Point A, Point B, Point C, Point D) {
 		this.A = A;
@@ -16,4 +16,14 @@ public class Parallelogram {
     public double perimeter(){
         return A.distance(B) + B.distance(C) + C.distance(D) + D.distance(A);
     }
+
+    public double area(){
+        double abx = B.x - A.x;
+        double adx = D.x - A.x;
+        double aby = B.y - A.y;
+        double ady = D.y - A.y;
+        return Math.abs(abx * ady - aby * adx);
+    }
+
+
 }
