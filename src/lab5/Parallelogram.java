7e63d39619f60b903fc3c4eb5d9f2fae4eb2ed04
Perryyyyyyyy <qiu13328224715@outlook.com>
2025-10-24 16:02:14 -0400

    feat(parallelogram): add class skeleton with 4 points and ctor

--- src/lab5/Parallelogram.java
+++ src/lab5/Parallelogram.java
@@ -1,5 +1,15 @@
 package lab5;
 
 public class Parallelogram {
-    
+    Point A; // bottom-left
+    Point B; // top-left
+    Point C; // top-right
+    Point D; // bottom-right
+	
+	public Parallelogram(Point A, Point B, Point C, Point D) {
+		this.A = A;
+		this.B = B;
+		this.C = C;
+		this.D = D;
+	}
 }
